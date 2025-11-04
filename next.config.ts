24f3577eb296fc3c1cb92201a4b3b2af62554332
Jose-11-2001 <josephmbukwa2001@gmail.com>
2025-11-04 23:55:10 +0200

    add net..

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+//import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+//const nextConfig: NextConfig = {
   /* config options here */
-};
+//};
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  // Optional: Add these for better performance
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+}
+
+module.exports = nextConfig
